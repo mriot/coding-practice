4f401f40debad098d7e914e1ec7f9757b5940e88
Markus <kremer.markus1996@gmail.com>
2023-01-18 00:39:33 +0100

    allow actual cli usage

--- cheat-table-hierarchy-dumper/main.py
+++ cheat-table-hierarchy-dumper/main.py
@@ -1,15 +1,12 @@
 import xml.etree.ElementTree as ET
 import sys
 
+
 # allow output redirect (>) into files on windows
 sys.stdout.reconfigure(encoding="utf-8")
 
-tree = ET.parse("PATH_TO_CHEAT_TABLE.CT")
-root = tree.getroot()
 
-entries_root = root.find("CheatEntries")
-
-def loop(node, depth = 0, guideline_levels = []):
+def dump(node, depth = 0, guideline_levels = []):
     for index, elem in enumerate(node):
         if elem.tag == "CheatEntry":
             caption           = elem.find("Description").text.replace('"', "")
@@ -52,11 +49,37 @@ def loop(node, depth = 0, guideline_levels = []):
                 # we decend one level deeper =  new guideline
                 guideline_levels.append(1)
 
-                loop(sub_cheat_entries, depth + 1, guideline_levels)
+                dump(sub_cheat_entries, depth + 1, guideline_levels)
 
                 # we ascend a level and want to remove the last guideline
                 if len(guideline_levels) > 0:
                     guideline_levels.pop()
 
 
-loop(entries_root)
+def main():
+    try:
+        filepath = sys.argv[1]
+
+        tree = ET.parse(filepath)
+        root = tree.getroot()
+
+        entries_root = root.find("CheatEntries")
+
+        dump(entries_root)
+
+    except IndexError:
+        print("Error: Please specify the path to a file as the first argument.")
+
+    except ET.ParseError:
+        print(f"Error: The file '{filepath}' could not be parsed.")
+    
+    except FileNotFoundError:
+        print(f"Error: The file '{filepath}' could not be found.")
+
+    except IsADirectoryError:
+        print(f"Error: '{filepath}' is a directory.")
+
+    except OSError:
+        print(f"Error: '{filepath}' is not a valid argument. Wildcards are not supported.")
+
+main()
